721e8aa4517e6161f277d287caad30b20884722f
Rano Veder <r.r.veder@student.tudelft.nl>
2017-04-03 01:04:41 +0200

    Updated readme and updated script to cycle through all files in the template folder

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Arrays
 Place to share PlaceBot (or whatever bot we decide to use) configs/arrays.
+
+This repository is meant to quickly generate reference arrays from all templates pictures in the template folder. By running the script after Template images have been created/updated, the program will create new Array files which can be used by the bot (still needs to be implemented) as reference for placing pixels.
+
+TODO list:
+
+Add upper-left reference positions for all arrays 
--- bitmaptoarray.py
+++ bitmaptoarray.py
@@ -6,7 +6,10 @@
 from PIL import Image
 import numpy as np
 import json
+import os
 
+Array_Path = "./Arrays/"
+Template_Path = "./Templates/"
 #Color Scheme
 #INDEX : COLOR
 # 0 : White
@@ -25,6 +28,7 @@ import json
 # 13 : Blue
 # 14 : Pink-Purple
 # 15 : Purple
+#16 : Dummy
 
 Color_List = [
     (255, 255, 255),
@@ -42,41 +46,50 @@ Color_List = [
     (0, 131, 199),
     (0, 0, 234),
     (207, 110, 228),
-    (130, 0, 128)
+    (130, 0, 128),
+    (54, 199, 57)
     ]
 
-# Specify image location to read later on
-Image_Path = raw_input("Enter image path/location: ")
-try:
-    # Try to read image using pil and convert array values to RGB
-    PIL_Image = Image.open(Image_Path).convert("RGB")
-except:
-    print "Something went wrong with opening the image, pls check your path again"
-    exit()
-# Get Width and height
-Width, Height = PIL_Image.size
-# Convert PIL object to pixel array
-Pixel_Array = PIL_Image.load()
-# Init a new numpy array to store color indices
-Final_Array = np.zeros((Width, Height), dtype='int')
 
-#Loop through image
-for i in range(Width):
-    for j in range(Height):
-        #Get color of selected pixel
-        color = Pixel_Array[i, j]
-        try:
-            #return the corresponding index in the newly created numpy array (on same position)
-            Final_Array[i, j] = Color_List.index(color)
-        except:
-            #In case the color is different for some reason (Not an r/place image), Throw error
-            print "Color value was not in list"
-            exit()
+def GenerateArrays(Image_Path, Filename):
+    try:
+        # Try to read image using pil and convert array values to RGB
+        PIL_Image = Image.open(Image_Path).convert("RGB")
+    except:
+        print "Something went wrong with opening the image, pls check your path again"
+        exit()
+    # Get Width and height
+    Width, Height = PIL_Image.size
+    # Convert PIL object to pixel array
+    Pixel_Array = PIL_Image.load()
+    # Init a new numpy array to store color indices
+    Final_Array = np.zeros((Height, Width), dtype='int')
 
-# Save array for use in python
-np.savetxt(Image_Path + ".Array",Final_Array, fmt='%i', delimiter='\t', header="Import in python using numpy.genfromtxt('PATH/FILENAME', skip_header=3 ,delimiter='\\t', dtype='int')\n\n")
-#Save array for use in javascript (JSON)
-with open(Image_Path + ".JSON", "w") as JSON_File:
-    json.dump(Final_Array.tolist(), JSON_File)
-#ALL DONE
+    #Loop through image
+    for i in range(Width):
+        for j in range(Height):
+            #Get color of selected pixel
+            color = Pixel_Array[i, j]
+            try:
+                #return the corresponding index in the newly created numpy array (on same position)
+                Final_Array[j, i] = Color_List.index(color)
+            except:
+                #In case the color is different for some reason (Not an r/place image), Throw error
+                print "Color value was not in list: ", color, Color_List.index(color), i, j
+            # exit()
+
+    # Save array for use in python
+    np.savetxt(Array_Path + Filename + ".Array",Final_Array, fmt='%i', delimiter='\t', header="Import in python using numpy.genfromtxt('PATH/FILENAME', skip_header=3 ,delimiter='\\t', dtype='int')\n\n")
+    #Save array for use in javascript (JSON)
+    with open(Array_Path + Filename + ".JSON", "w") as JSON_File:
+        json.dump(Final_Array.tolist(), JSON_File)
+
+
+#Loop Through Files
+for fn in os.listdir(Template_Path):
+    GenerateArrays(Template_Path + fn, fn)
+
+
+
+
 print "Done!, Saved file!"
